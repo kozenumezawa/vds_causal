a8060eef012d2493a77fefbfc202d1d0e027745b
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-09-29 18:46:25 +0900

    Update:

--- R/ccm_SandT.r
+++ R/ccm_SandT.r
@@ -2,6 +2,12 @@ library(rEDM)
 library(jsonlite)
 library(multispatialCCM)
 
+showdata <- function(Accm, Bccm) {
+  plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
+  lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
+  legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
+}
+
 determineEmbeddingDimension <- function(data) {
   lib <- c(1, 50)
   pred <- c(90, 212)  
@@ -25,6 +31,13 @@ identifyingNonlinearity <- function(data, Em) {
   plot(smap_output$theta, smap_output$rho, type = "l", xlab = "Nonlinearity (theta)", ylab = "Forecast Skill (rho)")
 }
 
+drawCCM <- function(CCM_boot_A, CCM_boot_B) {
+  plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
+  plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
+  lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
+  legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+}
+
 data = fromJSON("../python/data.json")
 
 index <- 1
@@ -32,35 +45,26 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
-
+showdata(Accm, Bccm)
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
 E_A = 2
 determineEmbeddingDimension(Bccm)
 E_B = 5
-
 # Prediction Decay
 predictionDeacy(data = Accm, Em = E_A)
 predictionDeacy(data = Bccm, Em = E_B)
 TAU = 1
-
 # Identifying Nonlinearity
 identifyingNonlinearity(data = Accm, Em = E_A)
 identifyingNonlinearity(data = Bccm, Em = E_B)
-
 # CCM(use multispatialCCM)
 signal_A_out<-SSR_check_signal(A=Accm, E=E_A, tau=TAU, predsteplist=1:10)
 signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
 
 # ---------------------------------------------------------------
 index <- 500
@@ -68,9 +72,7 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
+showdata(Accm, Bccm)
 
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
@@ -92,10 +94,7 @@ signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
 
 # ---------------------------------------------------------------
 index <- 1000
@@ -103,9 +102,7 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
+showdata(Accm, Bccm)
 
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
@@ -128,10 +125,7 @@ signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
 
 # ---------------------------------------------------------------
 index <- 9000
@@ -139,9 +133,7 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
+showdata(Accm, Bccm)
 
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
@@ -164,10 +156,7 @@ signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
 
 # ---------------------------------------------------------------
 index <- 2601
@@ -175,9 +164,7 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
+showdata(Accm, Bccm)
 
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
@@ -200,10 +187,7 @@ signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
 
 # ---------------------------------------------------------------
 index <- 130
@@ -211,10 +195,7 @@ Accm <- as.numeric(unlist(data$data[index,]$s))
 Bccm <- as.numeric(unlist(data$data[index,]$t))
 
 # show data
-plot(Accm, type="l", col=1, lwd=2, xlim=c(0, 212), ylim=c(0,1), xlab="time step", ylab="Normalized Value", cex.lab = 1.5)
-lines(Bccm, type="l", col=2, lty=2, lwd=2, cex.lab = 1.5)
-legend("topleft", c("Salinity", "Temprature"), cex=1.5, lty=c(1,2), col=c(1,2), lwd=2, bty="n")
-
+showdata(Accm, Bccm)
 # determine Embedding Dimension
 determineEmbeddingDimension(Accm)
 E_A = 1
@@ -227,13 +208,8 @@ predictionDeacy(data = Bccm, Em = E_B)
 TAU = 1
 
 # Identifying Nonlinearity
-smap_output <- s_map(Accm, lib, pred, E=E_A)
-par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(smap_output$theta, smap_output$rho, type = "l", xlab = "Nonlinearity (theta)", ylab = "Forecast Skill (rho)")
-
-smap_output <- s_map(Bccm, lib, pred, E=E_B)
-par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(smap_output$theta, smap_output$rho, type = "l", xlab = "Nonlinearity (theta)", ylab = "Forecast Skill (rho)")
+identifyingNonlinearity(data = Accm, Em = E_A)
+identifyingNonlinearity(data = Bccm, Em = E_B)
 
 # CCM(use multispatialCCM)
 signal_A_out<-SSR_check_signal(A=Accm, E=E_A, tau=TAU, predsteplist=1:10)
@@ -241,9 +217,4 @@ signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
 CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
 CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
 (CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
-
-# ---------------------------------------------------------------
+drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
