e6c613983b2b794b1ca4c97b9af3c0b405605daf
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-09-29 22:33:20 +0900

    Add:

--- logistic/python/visualize_test.py
+++ logistic/python/visualize_test.py
@@ -27,23 +27,43 @@ W2 = numpy.load('../npy/result_W2.npy')
 b2 = numpy.load('../npy/result_b2.npy')
 
 times = [i for i in range(TIME_STEP_2)]
+DATANUM = testdata.shape[0]
 
-y_ = numpy.matmul(testdata[1], W1) + b1
-y_activate = y_ / (1 + numpy.absolute(y_))
-y = numpy.maximum(numpy.matmul(y_activate, W2) + b2, 0)
+xW = numpy.zeros((DATANUM, b1.shape[0]), dtype=numpy.float32)
+y_ = numpy.zeros((DATANUM, b1.shape[0]), dtype=numpy.float32)
+y_activate = numpy.zeros((DATANUM, b1.shape[0]), dtype=numpy.float32)
+y = numpy.zeros((DATANUM, b2.shape[0]), dtype=numpy.float32)
 
-print(y.shape)
-plt.subplot(2, 1, 1)
-plt.plot(times, testdata[5], color='r', lw=2)
-plt.plot(times, y, color='g', lw=1)
-#
-# plt.subplot(2, 1, 2)
-# output = y.eval(session=sess, feed_dict={x: [testdata[8]], keep_prob: 1.0})
-# plt.plot(times, testdata[8], color='r', lw=2)
-# plt.plot(times, output[0], color='g', lw=1)
+# visualize input and output
+for i in range(0, DATANUM):
+    xW[i] = numpy.matmul(testdata[i], W1)
+    y_[i] = xW[i] + b1
+    y_activate[i] = y_[i] / (1 + numpy.absolute(y_[i]))
+    y[i] = numpy.maximum(numpy.matmul(y_activate[i], W2) + b2, 0)
 
-# plt.subplot(3, 2, 3)
-# plt.subplot(3, 2, 4)
-# plt.subplot(3, 2, 5)
-# plt.subplot(3, 2, 6)
+    plt.subplot(9, 1, i+1)
+    plt.plot(times, testdata[i], color='r', lw=2)
+    plt.plot(times, y[i], color='g', lw=1)
 plt.show()
+
+# visualize Wx
+number = [i for i in range(b1.shape[0])]
+plt.plot(number, xW[0], color='r', lw=2)
+plt.plot(number, xW[2], color='c', lw=2)
+plt.plot(number, xW[4], color='g', lw=2)
+plt.plot(number, xW[6], color='m', lw=2)
+plt.plot(number, xW[8], color='b', lw=2)
+plt.show()
+
+# 3, 8, 14, 18
+W = numpy.transpose(W1)
+plt.plot(times, W[8], color='g', lw=2)
+plt.plot(times, W[14], color='b', lw=2)
+print(W[3])
+print(W[8])
+print(W[14])
+plt.show()
+
+# visualize b1
+# plt.plot(number, b1, color='b', lw=2)
+# plt.show()
