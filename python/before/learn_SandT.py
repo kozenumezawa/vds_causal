2870e2b633ec989cfd30697f30d6d34b9aa55243
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-09-29 15:20:30 +0900

    Add:

--- python/before/learn_SandT.py
+++ python/before/learn_SandT.py
@@ -21,15 +21,18 @@ def main():
     data = numpy.zeros((rawdata.shape[0] * 2, rawdata.shape[2] * 2),
                        dtype=numpy.float32)
     st = numpy.zeros((rawdata.shape[0], rawdata.shape[2] * 2), dtype=numpy.float32)
+    ts = numpy.zeros((rawdata.shape[0], rawdata.shape[2] * 2), dtype=numpy.float32)
 
     for i in range(rawdata.shape[0]):
         st[i, :rawdata.shape[2]] = rawdata[i, 0]          #   S(salinity)
         st[i, rawdata.shape[2]:] = rawdata[i, 1]          #   T(water temperature)
+        ts[i, :rawdata.shape[2]] = rawdata[i, 1]      #   T(salinity)
+        ts[i, rawdata.shape[2]:] = rawdata[i, 0]      #   S(water temperature)
 
     # print(st.shape) = (10100, 424)
     PIXELS = data.shape[1]  # = 424
     H = 25
-    BATCH_SIZE = 1
+    BATCH_SIZE = 2
     DROP_OUT_RATE = 0.5
 
     x = tf.placeholder(tf.float32, [BATCH_SIZE, PIXELS], name='x')
@@ -37,8 +40,8 @@ def main():
     W = weight_variable((PIXELS, H), 'W')
     b1 = bias_variable([H], 'b1')
 
-    # h = tf.nn.softsign(tf.matmul(x, W) + b1)
-    h = tf.nn.sigmoid(tf.matmul(x, W) + b1)
+    h = tf.nn.softsign(tf.matmul(x, W) + b1)
+    # h = tf.nn.sigmoid(tf.matmul(x, W) + b1)
     # h = tf.matmul(x, W) + b1
     keep_prob = tf.placeholder("float", name='keep_prob')
     h_drop = tf.nn.dropout(h, keep_prob)
@@ -59,7 +62,7 @@ def main():
     summary_writer = tf.train.SummaryWriter('summary/l2_loss', graph=sess.graph)
 
     for step in range(5001):
-        inputdata = numpy.array([st[step]])
+        inputdata = numpy.array([st[step], ts[step]])
         sess.run(train_step,
                  feed_dict={x: inputdata, keep_prob: (1 - DROP_OUT_RATE)})
         summary_op = tf.merge_all_summaries()
@@ -72,8 +75,12 @@ def main():
         if step % 1000 == 0:
             times = [i for i in range(PIXELS)]
             output = y.eval(session=sess, feed_dict={x: inputdata, keep_prob: 1.0})
+            plt.subplot(2, 1, 1)
             plt.plot(times, inputdata[0], color='r', lw=2)
             plt.plot(times, output[0], color='g', lw=1)
+            plt.subplot(2, 1, 2)
+            plt.plot(times, inputdata[1], color='r', lw=2)
+            plt.plot(times, output[1], color='g', lw=1)
             plt.show()
     result = sess.run(W2)
     numpy.save('result.npy', result)
