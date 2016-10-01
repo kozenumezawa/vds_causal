9803d7e0e0ee27856b927b91e995e50a4f5f6e37
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-10-01 15:05:45 +0900

    Update:

--- R/ccm_test.r
+++ R/ccm_test.r
@@ -13,6 +13,16 @@ determineEmbeddingDimension <- function(data) {
   pred <- c(90, 212)  
   simplex_output <- simplex(data, lib, pred)
   plot(simplex_output$E, simplex_output$rho, type = "l", xlab = "Embedding Dimension (E)", ylab = "Forecast Skill (rho)")
+  
+  max_index <- 1
+  max <- simplex_output$rho[1]
+  for (i in 2:length(simplex_output$rho)) {
+    if (simplex_output$rho[i] > max) {
+      max_index <- i
+      max <- simplex_output$rho[i]
+    }  
+  }
+  return(max_index)
 }
 
 predictionDeacy <- function(data, Em) {
@@ -31,11 +41,18 @@ identifyingNonlinearity <- function(data, Em) {
   plot(smap_output$theta, smap_output$rho, type = "l", xlab = "Nonlinearity (theta)", ylab = "Forecast Skill (rho)")
 }
 
-drawCCM <- function(CCM_boot_A, CCM_boot_B) {
-  plotxlimits<-range(c(CCM_boot_A$Lobs, CCM_boot_B$Lobs))
-  plot(CCM_boot_A$Lobs, CCM_boot_A$rho, type="l", col=1, lwd=2, xlim=c(plotxlimits[1], plotxlimits[2]), ylim=c(0,1), xlab="Library Size", ylab="Cross Map Skill (rho)", cex.lab = 1.5)
-  lines(CCM_boot_B$Lobs, CCM_boot_B$rho, type="l", col=2, lty=2, lwd=2)
-  legend("topleft", c("Salinity causes Temprature", "Temprature causes Salinituy"), lty=c(1,2), col=c(1,2), lwd=2, bty="n", cex=1.2)
+drawCCM <- function(Accm, Bccm, E_A, E_B, TAU) {
+  Accm_Bccm <- data.frame(Accm=Accm, Bccm=Bccm)
+  Accm_xmap_Bccm <- ccm(Accm_Bccm, E = E_A, lib_column = "Accm", tau = TAU,
+                        target_column = "Bccm", lib_sizes = seq(10, 200, by = 10), random_libs = TRUE)
+  Bccm_xmap_Accm <- ccm(Accm_Bccm, E = E_B, lib_column = "Bccm", tau = TAU,
+                        target_column = "Accm", lib_sizes = seq(10, 200, by = 10), random_libs = TRUE)
+  Accm_xmap_Bccm_means <- ccm_means(Accm_xmap_Bccm)
+  Bccm_xmap_Accm_means <- ccm_means(Bccm_xmap_Accm)
+  par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
+  plot(Accm_xmap_Bccm_means$lib_size, pmax(0, Accm_xmap_Bccm_means$rho), type = "l", col = "red", xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1))
+  lines(Bccm_xmap_Accm_means$lib_size, pmax(0, Bccm_xmap_Accm_means$rho), col = "blue")
+  legend(x = "topleft", legend = c("Accm xmap Bccm", "Bccm xmap Accm"), col = c("red", "blue"), lwd = 1, inset = 0.02, cex = 0.8)
 }
 
 data = fromJSON("../python/data.json")
@@ -47,10 +64,8 @@ Bccm <- as.numeric(unlist(data$data[index,]$t))
 # show data
 showdata(Accm, Bccm)
 # determine Embedding Dimension
-determineEmbeddingDimension(Accm)
-E_A = 2
-determineEmbeddingDimension(Bccm)
-E_B = 5
+E_A <- determineEmbeddingDimension(Accm)
+E_B <- determineEmbeddingDimension(Bccm)
 # Prediction Decay
 predictionDeacy(data = Accm, Em = E_A)
 predictionDeacy(data = Bccm, Em = E_B)
@@ -58,27 +73,34 @@ TAU = 1
 # Identifying Nonlinearity
 identifyingNonlinearity(data = Accm, Em = E_A)
 identifyingNonlinearity(data = Bccm, Em = E_B)
-# CCM(use multispatialCCM)
-signal_A_out<-SSR_check_signal(A=Accm, E=E_A, tau=TAU, predsteplist=1:10)
-signal_B_out<-SSR_check_signal(A=Bccm, E=E_B, tau=TAU, predsteplist=1:10)
-CCM_boot_A<-CCM_boot(Accm, Bccm, E_A, tau=TAU, iterations=30)
-CCM_boot_B<-CCM_boot(Bccm, Accm, E_B, tau=TAU, iterations=30)
-(CCM_significance_test<-ccmtest(CCM_boot_A, CCM_boot_B))
-drawCCM(CCM_boot_A = CCM_boot_A, CCM_boot_B = CCM_boot_B)
+# draw CCM
+drawCCM(Accm = Accm, Bccm = Bccm, E_A = E_A, E_B = E_B, TAU = TAU)
 
-# create data frame from datasets
-Accm_Bccm <- data.frame(Accm=Accm, Bccm=Bccm)
-Accm_xmap_Bccm <- ccm(Accm_Bccm, E = E_A, lib_column = "Accm", tau = TAU,
-                        target_column = "Bccm", lib_sizes = seq(10, 200, by = 10), random_libs = FALSE)
-Bccm_xmap_Accm <- ccm(Accm_Bccm, E = E_B, lib_column = "Bccm", tau = TAU,
-                      target_column = "Accm", lib_sizes = seq(10, 200, by = 10), random_libs = FALSE)
+#---twin surrogate---
+Heaviside <- function(x) {
+  if (x > 0) {
+    return (1)
+  }
+  return (0)
+}
+# create trajectory vector form attractor
+X_DIM <- E_A
+BACK_MAX <- (X_DIM - 1) * TAU
+X_N <- length(Accm) - BACK_MAX # length of x
+x <- array(0, dim=c(X_N, X_DIM))
+for (t in 1 : X_N) {
+  for(j in 1:X_DIM) {
+    x[t,j] <- Accm[(t + BACK_MAX) - (j - 1) * TAU]
+  }
+}
 
-Accm_xmap_Bccm_means <- ccm_means(Accm_xmap_Bccm)
-Bccm_xmap_Accm_means <- ccm_means(Bccm_xmap_Accm)
-par(mar = c(4, 4, 1, 1), mgp = c(2.5, 1, 0))
-plot(Accm_xmap_Bccm_means$lib_size, pmax(0, Accm_xmap_Bccm_means$rho), type = "l", col = "red", 
-     xlab = "Library Size", ylab = "Cross Map Skill (rho)", ylim = c(0, 1))
-lines(Bccm_xmap_Accm_means$lib_size, pmax(0, Bccm_xmap_Accm_means$rho), col = "blue")
-legend(x = "topleft", legend = c("Accm xmap Bccm", "Bccm xmap Accm"), col = c("red", 
-                                                                                  "blue"), lwd = 1, inset = 0.02, cex = 0.8)
+# cauculate reccurrence matrix
+delta <- 0.125
+R <- array(0, dim=c(X_N, X_N))
+for (i in 1 : X_N) {
+  for(j in 1 : X_N) {
+    R[i,j] <- Heaviside(delta - max(abs(x[i,] - x[j,])))
+  }
+}
 
+# create twin surrogates data
